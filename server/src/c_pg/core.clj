fff33a0f1e48e0f5a7d48810e4d17739878c8603
u4bi <admin@naver.id>
2018-05-07 00:55:09 +0900

    Added PostgreSQL query.

--- server/src/c_pg/core.clj
+++ server/src/c_pg/core.clj
@@ -3,17 +3,73 @@
   (:require [compojure.core :refer :all]
             [compojure.handler :as h]
             [compojure.route :as route]
-            [ring.middleware.json :refer [wrap-json-response]]))
+            [ring.middleware.json :as m-json]
+            [clojure.java.jdbc :as sql]))
+
+(def conn {:dbtype "postgresql"
+           :dbname "postgres"
+           :host "localhost"
+           :port 5432})
+
+(defn response-data [data]
+  (cond
+    (identity data) data
+    :else {:message "data not found"}))
+
+(defn get-users []
+  (sql/query conn
+             ["SELECT * FROM _user"]))
+
+(defn get-user [id]
+  (sql/query conn 
+             ["SELECT 
+                  *
+                FROM 
+                    _user
+                WHERE 
+                    id = ?" id]
+             {:result-set-fn first}))
+
+(defn insert-user [name pay age]
+  (first
+   (sql/insert! conn
+                :_user {
+                        :name name
+                        :pay pay
+                        :age age})))
+
+(defn update-user [id name pay age]
+  (let [is-updated (first
+                    (sql/update! conn
+                                 :_user
+                                 {
+                                  :name name
+                                  :pay pay
+                                  :age age}
+                                 ["id = ?" id]))]
+    (if is-updated {
+                    :id id
+                    :name name
+                    :pay pay
+                    :age age} nil)))
+
+(defn delete-user [id]
+  (let [is-deleted (first
+                    (sql/delete! conn
+                                 :_user
+                                 ["id = ?" id]))]
+    (if is-deleted {:id id} nil)))
 
 (defroutes handler
            (context "/user" []
-                    (GET "/" [] (response {:message "get users"})) 
-                    (GET "/:id" [id] (response {:message (str "get user id - " id)})) 
-                    (POST "/" [name pay age] (response {:message (str "insert user - " name " / " pay " / " age)}))
-                    (PUT "/:id" [id] (response {:message (str "update user id - " id)})) 
-                    (DELETE "/:id" [id] (response {:message (str "delete id - " id)})))
+                    (GET "/" [] (response (response-data (get-users))))
+                    (GET "/:id" [id] (response (response-data (get-user (read-string id)))))
+                    (POST "/" [name pay age] (response (response-data (insert-user name pay age))))
+                    (PUT "/:id" [id name pay age] (response (response-data (update-user (read-string id) name pay age))))
+                    (DELETE "/:id" [id] (response (response-data (delete-user (read-string id))))))
            (route/not-found (response {:message "not found"})))
 
 (def app
-  (wrap-json-response 
-   (h/api handler)))
+  (-> (h/api handler)
+      (m-json/wrap-json-params)
+      (m-json/wrap-json-response)))
